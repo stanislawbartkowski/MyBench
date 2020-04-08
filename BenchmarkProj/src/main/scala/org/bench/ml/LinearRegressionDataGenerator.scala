d1a73d6f55ce87f65908711db1386a4b25b66550
sbartkowski <stanislawbartkowski@gmail.com>
2020-04-08 14:50:33 +0200

    Linear Regression

--- BenchmarkProj/src/main/scala/org/bench/ml/LinearRegressionDataGenerator.scala
+++ BenchmarkProj/src/main/scala/org/bench/ml/LinearRegressionDataGenerator.scala
@@ -20,17 +20,19 @@ package org.bench.ml
 
 import scala.util.Random
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.mllib.linalg.Vectors
+import com.github.fommil.netlib.BLAS.{getInstance => blas}
 import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.rdd.RDD
 import org.rogach.scallop.ScallopConf
 
 /**
  * :: DeveloperApi ::
- * Generate test data for LogisticRegression. This class chooses positive labels
- * with probability `probOne` and scales features for positive examples by `eps`.
+ * Generate sample data for Linear Regression. This class
+ * generates uniformly random values for each feature and
+ * adds Gaussian noise with mean 'eps' to the label 'Y'.
  */
-object LogisticRegressionDataGenerator {
+object LinearRegressionDataGenerator {
 
   class Params(arguments: Seq[String]) extends ScallopConf(arguments) {
 
@@ -45,41 +47,56 @@ object LogisticRegressionDataGenerator {
     For usage see below:
     """)
     private val odataPath = opt[String]("dataPath", required = true)
-    val onumExamples =  opt[Int]("numExamples")
+    val onumExamples = opt[Int]("numExamples")
     val onumFeatures = opt[Int]("numFeatures")
     verify()
 
     val dataPath: String = odataPath.getOrElse("")
-    val numExamples = onumExamples.getOrElse(200000)
-    val numFeatures : Int = onumFeatures.getOrElse(20)
+    val numExamples = onumExamples.getOrElse(1000)
+    val numFeatures: Int = onumFeatures.getOrElse(50)
   }
 
 
   /**
-   * Generate an RDD containing test data for LogisticRegression.
+   * Generate an RDD containing sample data for Linear Regression.
    *
-   * @param sc SparkContext to use for creating the RDD.
-   * @param nexamples Number of examples that will be contained in the RDD.
-   * @param nfeatures Number of features to generate for each example.
-   * @param eps Epsilon factor by which positive examples are scaled.
-   * @param nparts Number of partitions of the generated RDD. Default value is 2.
-   * @param probOne Probability that a label is 1 (and not 0). Default value is 0.5.
+   * @param sc          SparkContext to use for creating the RDD.
+   * @param numExamples Number of examples that will be contained in the RDD.
+   * @param numFeatures Numer of features to gnerate for each example.
+   * @param eps         Epsilon factor by which examples are scaled.
+   * @param numParts    Number of partitions of the generated RDD. Default value is 3.
+   * @param seed        Random seed for each partition
    */
-  def generateLogisticRDD(
-                           sc: SparkContext,
-                           nexamples: Int,
-                           nfeatures: Int,
-                           eps: Double,
-                           nparts: Int = 2,
-                           probOne: Double = 0.5): RDD[LabeledPoint] = {
-    val data = sc.parallelize(0 until nexamples, nparts).map { idx =>
-      val rnd = new Random(42 + idx)
+  def generateLinearRDD(
+                         sc: SparkContext,
+                         numExamples: Int,
+                         numFeatures: Int,
+                         eps: Double,
+                         numParts: Int = 3,
+                         seed: Long = System.currentTimeMillis()): RDD[LabeledPoint] = {
+    val random = new Random()
+    // Random values distributed uniformly in [-0.5, 0.5]
+    val weights = Array.fill(numFeatures)(random.nextDouble() - 0.5)
 
-      val y = if (idx % 2 == 0) 0.0 else 1.0
-      val x = Array.fill[Double](nfeatures) {
-        rnd.nextGaussian() + (y * eps)
-      }
-      LabeledPoint(y, Vectors.dense(x))
+    val data: RDD[LabeledPoint] = sc.parallelize(0 until numExamples, numParts).mapPartitions {
+      part =>
+        val rnd = new Random(seed)
+        // mean for each feature
+        val xMean = Array.fill[Double](weights.length)(0.0)
+        // variance for each feature
+        val xVariance = Array.fill[Double](weights.length)(1.0 / 3.0)
+
+        def rndElement(i: Int) = {
+          (rnd.nextDouble() - 0.5) * math.sqrt(12.0 * xVariance(i)) + xMean(i)
+        }
+
+        part.map { _ =>
+          val features = Vectors.dense(weights.indices.map {
+            rndElement(_)
+          }.toArray)
+          val label = blas.ddot(weights.length, weights, 1, features.toArray, 1) + eps * rnd.nextGaussian()
+          LabeledPoint(label, features)
+        }
     }
     data
   }
@@ -87,31 +104,17 @@ object LogisticRegressionDataGenerator {
   def main(args: Array[String]) {
     val params = new Params(args)
 
-    val conf = new SparkConf().setAppName("LogisticRegressionDataGenerator")
+    val conf = new SparkConf().setAppName("LinearRegressionDataGenerator")
     Common.setMaster(conf)
     val sc = new SparkContext(conf)
-    val numPartitions = Common.getNumOfPartitons(sc)
 
     var outputPath = params.dataPath
-    var numExamples: Int = params.numExamples
+    var numExamples = params.numExamples
     var numFeatures: Int = params.numFeatures
-    val eps = 3
+    val numPartitions = Common.getNumOfPartitons(sc)
+    var eps: Double = 1.0
 
-    if (args.length == 3) {
-      outputPath = args(0)
-      numExamples = args(1).toInt
-      numFeatures = args(2).toInt
-      println(s"Output Path: $outputPath")
-      println(s"Num of Examples: $numExamples")
-      println(s"Num of Features: $numFeatures")
-    } else {
-      System.err.println(
-        s"Usage: $LogisticRegressionDataGenerator <OUTPUT_PATH> <NUM_EXAMPLES> <NUM_FEATURES>"
-      )
-      System.exit(1)
-    }
-
-    val data = generateLogisticRDD(sc, numExamples, numFeatures, eps, numPartitions)
+    val data = generateLinearRDD(sc, numExamples, numFeatures, eps, numPartitions)
 
     data.saveAsObjectFile(outputPath)
 
